547b11aeddc5a72c2cbc598cf1a39e412813dcf1
sumit87814 <sumit87814@gmail.com>
2026-02-10 09:35:13 +0530

    thriviaBot

---
+++ thriviaBot.js
@@ -0,0 +1,13 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Doctorbot";
+let botLocation= "Newyork";
+let favoriteLanguage = "Javascript";
+console.log("My name is " + botName +" and I live on " + botLocation +".");
+console.log("My favorite programming language is " + favoriteLanguage +".");
+let codingFact = "My best friend and I met in college in " + favoriteLanguage + " class.";
+console.log(codingFact);
+codingFact = "I made an A in " + favoriteLanguage + " class.";
+console.log(codingFact);
+codingFact = "I helped invent " + favoriteLanguage + ". - Just kidding!"
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
